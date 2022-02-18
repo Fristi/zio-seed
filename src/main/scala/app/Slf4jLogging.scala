70ee1e6addd5f3e181363be15786ff1220231f85
Mark <mark@vectos.net>
2022-02-18 16:42:39 +0100

    Fix logging

--- src/main/scala/app/Slf4jLogging.scala
+++ src/main/scala/app/Slf4jLogging.scala
@@ -5,13 +5,10 @@ import zio.logging.{LogAnnotation, Logging}
 import zio.logging.slf4j.Slf4jLogger
 
 object Slf4jLogging {
-  val format = "[correlation-id = %s] %s"
+  val format = "%s"
 
   val env: ULayer[Logging] =
     Slf4jLogger.make{(context, message) =>
-      val correlationId = LogAnnotation.CorrelationId.render(
-        context.get(LogAnnotation.CorrelationId)
-      )
-      format.format(correlationId, message)
+      format.format(message)
     }
 }
