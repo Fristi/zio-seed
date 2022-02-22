c08da54631d54f39d429b0a2e2bc7754732f55e4
Mark <mark@vectos.net>
2022-02-22 08:44:24 +0100

    Different route setup

--- src/main/scala/app/Main.scala
+++ src/main/scala/app/Main.scala
@@ -1,9 +1,8 @@
 package app
 
 import cats.implicits._
-import org.http4s.HttpRoutes
 import org.http4s.blaze.server.BlazeServerBuilder
-import org.http4s.server.Router
+import org.http4s.{HttpApp, HttpRoutes}
 import sttp.capabilities.zio.ZioStreams
 import sttp.tapir.server.http4s.ztapir.ZHttp4sServerInterpreter
 import sttp.tapir.swagger.bundle.SwaggerInterpreter
@@ -25,30 +24,36 @@ object Main extends App {
   def nameEndpoint: ZServerEndpoint[Logging, ZioStreams] = Endpoints.namePerson.zServerLogic(namePerson)
 
   type Env = Logging
-
   type RuntimeEff[A] = RIO[Env with Clock with Blocking, A]
 
-  def routes: HttpRoutes[RuntimeEff] =
-    ZHttp4sServerInterpreter().from(List(countEndpoint.widen[Env], nameEndpoint.widen[Env])).toRoutes
+  def docs: HttpRoutes[RuntimeEff] =
+    ZHttp4sServerInterpreter()
+      .from(
+        SwaggerInterpreter(basePrefix = List("api")).fromEndpoints[RuntimeEff](
+            List(Endpoints.countCharacters, Endpoints.namePerson),
+            "ZIO seed",
+            "1.0"
+          )
+      )
+      .toRoutes
 
-
-  def endpoints = List(Endpoints.countCharacters, Endpoints.namePerson)
-
-  def docs: HttpRoutes[RuntimeEff] = ZHttp4sServerInterpreter()
-    .from(SwaggerInterpreter().fromEndpoints[RuntimeEff](endpoints, "ZIO seed", "1.0")).toRoutes
-
-  def serve[R <: Clock with Blocking](routes: HttpRoutes[RIO[R, *]]): ZIO[R, Throwable, Unit] =
+  def serve[R <: Clock with Blocking](routes: HttpApp[RIO[R, *]]): ZIO[R, Throwable, Unit] =
     ZIO.runtime[R].flatMap { implicit runtime =>
       BlazeServerBuilder[RIO[R, *]]
         .withExecutionContext(runtime.platform.executor.asEC)
         .bindHttp(8080, "0.0.0.0")
-        .withHttpApp(Router("/" -> routes).orNotFound)
+        .withHttpApp(routes)
         .withoutBanner
         .serve
         .compile
         .drain
     }
 
-  override def run(args: List[String]): URIO[ZEnv, ExitCode] =
-    serve(routes <+> docs).exitCode.provideLayer(Slf4jLogging.env ++ ZEnv.live)
+  override def run(args: List[String]): URIO[ZEnv, ExitCode] = {
+
+    val router =
+      ZHttp4sServerInterpreter().from(List(countEndpoint.widen[Env], nameEndpoint.widen[Env])).toRoutes <+> docs
+
+    serve(router.orNotFound).exitCode.provideLayer(Slf4jLogging.env ++ ZEnv.live)
+  }
 }
